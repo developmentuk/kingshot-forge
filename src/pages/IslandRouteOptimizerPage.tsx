85593d20c795b1cc9cde1a5bc36faa6c2ab179e6
developmentuk <clark.sim@gmail.com>
2026-08-06 13:20:08 +0100

    feat(island): load optimizer styles from page entry

--- src/pages/IslandRouteOptimizerPage.tsx
+++ src/pages/IslandRouteOptimizerPage.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import IslandRouteOptimizer from '../features/island-route/IslandRouteOptimizer.js'
+import '../features/island-route/islandRoute.css'
 
 export default function IslandRouteOptimizerPage() {
   return <main className="island-route-page">
