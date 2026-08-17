5286edc42b984c5c3b9448c5a2a0bec2c8a7888c
developmentuk <clark.sim@gmail.com>
2026-08-17 16:50:46 +0100

    CONTENT-001: route Generation 6 guide through Forge app

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ import PublicPlayerProgressionPage from "./pages/PublicPlayerProgressionPage";
 import HeroShowcaseEditorPage from "./pages/HeroShowcaseEditorPage";
 import HeroCollectionPage from "./pages/HeroCollectionPage";
 import HeroCompanionPage from "./pages/HeroCompanionPage";
+import Generation6HeroGuidePage from "./pages/Generation6HeroGuidePage";
 import CompanionIndexPage from "./pages/CompanionIndexPage";
 import CompanionItemPage from "./pages/CompanionItemPage";
 import SearchPage from "./features/search/SearchPage";
@@ -141,6 +142,7 @@ function App() {
         <Route path="join/my-application" element={<MyContributorApplicationPage />} />
         <Route path="companion/heroes" element={<HeroCompanionPage />} />
         <Route path="companion/heroes/:heroId" element={<HeroCompanionPage />} />
+        <Route path="guides/kingshot-generation-6-heroes-yang-sophia-triton-guide" element={<Generation6HeroGuidePage />} />
 
         <Route path="operations" element={<WorkspaceRoute workspaceId="operations"><OperationsDashboardPage /></WorkspaceRoute>} />
         <Route path="operations/users" element={<WorkspaceRoute workspaceId="operations"><ProtectedRoute permission="users.read"><UserManagementPage /></ProtectedRoute></WorkspaceRoute>} />
