0a0e283e138c930d8676c6eeef6b32919378bb11
developmentuk <clark.sim@gmail.com>
2026-08-19 14:23:41 +0100

    fix(companion): combine governed item recovery phases

---
+++ shared/companion/itemGameplayCatalogue.ts
@@ -0,0 +1,19 @@
+import {
+  COMPANION_ITEM_GAMEPLAY_CONTENT,
+} from './itemGameplayContent.js'
+import {
+  COMPANION_ITEM_GAMEPLAY_RECOVERY_PHASE2,
+} from './itemGameplayRecoveryPhase2.js'
+
+export const COMPANION_ITEM_GAMEPLAY = Object.freeze({
+  ...COMPANION_ITEM_GAMEPLAY_CONTENT,
+  ...COMPANION_ITEM_GAMEPLAY_RECOVERY_PHASE2,
+})
+
+export const COMPANION_ITEM_GAMEPLAY_PHASE1_KEYS = Object.freeze(
+  Object.keys(COMPANION_ITEM_GAMEPLAY_CONTENT),
+)
+
+export const COMPANION_ITEM_GAMEPLAY_PHASE2_KEYS = Object.freeze(
+  Object.keys(COMPANION_ITEM_GAMEPLAY_RECOVERY_PHASE2),
+)
